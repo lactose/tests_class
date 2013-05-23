d09ad05a1ef5ecf3208797b3506d6237e7049632
Zachary Owen <dev@scrptr.com>
2013-05-22 20:46:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testsclass::Application.config.secret_token = 'a820b9896f6e31c6dc9452939d38df7e060f4e14fce2ac005a23cbb0679953509f8044d88c46ef49674d1b32af9b5cb2976f0359dde09e899df1cd57b78422e2'
